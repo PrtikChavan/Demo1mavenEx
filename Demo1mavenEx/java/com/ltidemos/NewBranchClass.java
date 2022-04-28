0bee70a22b5f206bac9d94274faf92c1601eeea9
PrtikChavan <chavanpratik410@gmail.com>
2022-04-28 15:02:57 +0530

    new branch

---
+++ Demo1mavenEx/java/com/ltidemos/NewBranchClass.java
@@ -0,0 +1,10 @@
+package com.ltidemos;
+
+public class NewBranchClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println(" new branch class");
+	}
+
+}
